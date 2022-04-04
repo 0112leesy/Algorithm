c56a8c02552c6eaa5fde918a542541783f186938
seyoung <0112leesy@naver.com>
2022-04-04 23:20:35 +0900

    22.04.04 : Heap_v4

---
+++ Heap/src/PriQue.java
@@ -0,0 +1,19 @@
+import java.util.PriorityQueue;
+
+public class PriQue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int형 priorityQueue 선언 (우선순위가 낮은 숫자 순)
+		PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+				
+		priorityQueue.add(3);     // priorityQueue 값 3 추가
+		priorityQueue.add(4);     // priorityQueue 값 4 추가
+		priorityQueue.add(1);     // priorityQueue 값 1 추가
+		priorityQueue.add(10);    // priorityQueue 값 10 추가
+		while(!priorityQueue.isEmpty()) {
+			System.out.println(priorityQueue.poll());
+		}		
+	}
+
+}
